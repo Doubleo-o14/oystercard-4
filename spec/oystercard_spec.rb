5da12a6515ebb94c70bcdc7ba55e7a9d29722321
olaleye <olaoduola01@gmail.com>
2021-10-26 15:44:34 +0100

    added top up

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,9 @@ describe Oystercard do
         card = Oystercard.new
         expect(card.balance).to eq(0)
     end
+    describe '#top_up' do
+      it "tops your card up" do
+        expect(subject).to respond_to(:top_up).with(1).argument
+      end
+    end
 end
