fa6855dc207263f9973a7344a2a1e9d07a8ad82c
Mark McLeod <markmcleod45@gmail.com>
2021-10-28 13:04:17 +0100

    refactor tests and male them pass

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,31 +1,27 @@
 class Oystercard
   MAXIMUM_BALANCE = 90
-  MININMUM_FARE = 1
-  attr_reader :balance
+  attr_reader :balance, :in_journey
   
   def initialize
     @balance = 0
     @in_journey = false
   end
 
-    def top_up(amount)
-        fail "Maximum balance exceeded" if @balance + amount > MAXIMUM_BALANCE
-        @balance += amount
-    end
+  def top_up(amount)
+    fail "Maximum balance exceeded" if @balance + amount > MAXIMUM_BALANCE
+      @balance += amount
+  end
 
-    def deduct(amount)
-        @balance -= amount
-    end
+  def deduct(amount)
+    @balance -= amount
+  end
 
-    def touch_in
-        fail "no funds" if balance < 1
-        @in_journey = true
-    end
+  def touch_in
+    fail "Insufficient balance on card" if balance < 1
+    @in_journey = true
+  end
 
-    def touch_out
-        @in_journey = false
-    end
-    
-    def in_journey?
-    end
+  def touch_out
+    @in_journey = false
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,53 +1,65 @@
-require 'oystercard'
+require './lib/oystercard'
 
 describe Oystercard do
 
-    describe '#balance' do
-      it "has a balance of 0 on initalization" do
-          card = Oystercard.new
-          expect(card.balance).to eq(0)
-      end
+  describe '#balance' do
+    it "has a balance of 0 on initalization" do
+      card = Oystercard.new
+      expect(card.balance).to eq(0)
     end
+  end
     
-    describe '#top_up' do
-      it "add money to your card up" do
-        expect { subject.top_up 10 }.to change { subject.balance }.by(10)
-      end
+  describe '#top_up' do
+    it "add money to your card up" do
+      expect { subject.top_up 10 }.to change { subject.balance }.by(10)
+    end
       
-      it "raise an error if balance exceeds maximum balance" do
-        maximum_balance = Oystercard::MAXIMUM_BALANCE
-        subject.top_up(maximum_balance)
-        expect{ subject.top_up(1) }.to raise_error "Maximum balance exceeded"
+    it "raise an error if balance exceeds maximum balance" do
+      maximum_balance = Oystercard::MAXIMUM_BALANCE
+      subject.top_up(maximum_balance)
+      expect{ subject.top_up(1) }.to raise_error "Maximum balance exceeded"
+    end
+  end
+
+  describe "#deduct" do
+    it "deducts 1 from balance" do
+      expect { subject.deduct(1) }.to change { subject.balance }.by(-1)
+    end
+  end
+
+  describe '#in_journey?' do
+    context "when oystercard is initalized it" do
+      it "is false" do
+        expect(subject.in_journey).to be false
+      end
+    end
+  end
+
+
+  describe "#touch in" do
+
+    context "when oystercard has been touched in" do
+      it "is true" do
+        subject.top_up(50)
+        subject.touch_in
+        expect(subject.in_journey).to be true
       end
     end
 
-    describe "#deduct" do
-      it "deducts minimum_fare from balance" do
-        minimum_fare = Oystercard::MININMUM_FARE
-        expect { subject.deduct(minimum_fare) }.to change { subject.balance }.by(-minimum_fare)
+    it "raise an error of insufficent funds" do
+      expect{ subject.touch_in }.to raise_error "Insufficient balance on card"
+    end
+  end
+
+  describe "#touch out" do
+
+    context "when oystercard has been touched out" do
+      it "is false" do
+        subject.top_up(50)
+        subject.touch_in
+        subject.touch_out
+        expect(subject.in_journey).to be false
       end
     end
-
-
-    #### REFACTORED UP TO HERE
-
-    describe '#in_journey?' do
-      context "when oystercard is initalized it" do
-        it "is false" do
-          expect(subject.in_journey?).to be false
-        end
-    end
+  end
 end
-
-#     describe "#touch in" do
-#       it "raise an error of insufficent funds" do
-#         expect{ subject.touch_in }.to raise_error "no funds"
-#       end
-#     end
-
-#     describe "#touch out" do
-#       it "leaving train station and ending journey" do
-#         expect(subject.touch_out).to eq false
-#       end
-#     end
-# end
